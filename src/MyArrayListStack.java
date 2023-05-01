6be0fc4e6f4fac14a62538aa97c8da545fe0b588
DaldenU <daldenuteryag@gmail.com>
2023-05-01 18:05:06 +0600

    Implementation of the push method and added comments

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -2,10 +2,13 @@ import java.util.ArrayList;
 import java.util.EmptyStackException;
 
 public class MyArrayListStack<T> {
-    private ArrayList<T> arrStack;
+    private ArrayList<T> arrStack; //Declaration of an ArrayList to hold the elements of the stack.
 
     public MyArrayListStack() {
-        arrStack = new ArrayList<>();
+        arrStack = new ArrayList<>(); //Initialisation of ArrayList in the constructor
     }
 
+    public void push(T element) {
+        arrStack.add(element); //adds an element to the top of the stack
+    }
 }
