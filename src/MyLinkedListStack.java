02a93822e0d1b2e95d048627c0fed0c596e044c8
DaldenU <daldenuteryag@gmail.com>
2023-05-01 18:30:01 +0600

    Creating the constructor for MyLinkedListStack

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -2,4 +2,8 @@ import java.util.LinkedList;
 import java.util.EmptyStackException;
 public class MyLinkedListStack<T> {
     private LinkedList<T> linkStack; //Declaration of an LinkedList to hold the elements of the stack.
+
+    public MyLinkedListStack() {
+        linkStack = new LinkedList<>(); ////Initialisation of LinkedList in the constructor
+    }
 }
