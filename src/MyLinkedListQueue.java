2cffaa4ad1750df990e9baffbca3d7859bb3e26a
DaldenU <daldenuteryag@gmail.com>
2023-05-01 19:10:34 +0600

    constructor for the class

--- src/MyLinkedListQueue.java
+++ src/MyLinkedListQueue.java
@@ -3,4 +3,8 @@ import java.util.NoSuchElementException;
 
 public class MyLinkedListQueue<T> {
     private LinkedList<T> queue; //Declaration of LinkedList to hold the elements of the queue.
+
+    public MyLinkedListQueue() {
+        queue = new LinkedList<>(); //Initialisation of LinkedList in the constructor.
+    }
 }
