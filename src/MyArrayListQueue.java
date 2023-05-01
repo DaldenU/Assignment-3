0342eace2f6522bf2fac722233d3462d1b9395ae
DaldenU <daldenuteryag@gmail.com>
2023-05-01 18:52:46 +0600

    constructor for class

--- src/MyArrayListQueue.java
+++ src/MyArrayListQueue.java
@@ -2,4 +2,8 @@ import java.util.ArrayList;
 import java.util.NoSuchElementException;
 public class MyArrayListQueue<T> {
     private ArrayList<T> queue; // Declare an ArrayList to hold the elements of the queue.
+
+    public MyArrayListQueue(){
+        queue = new ArrayList<>(); // Initialisation of ArrayList in the constructor.
+    }
 }
