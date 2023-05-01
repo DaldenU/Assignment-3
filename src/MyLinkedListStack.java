ec58187e509dd6e307aa8e510b4c943325779aea
DaldenU <daldenuteryag@gmail.com>
2023-05-01 18:28:36 +0600

    Declare an instance variable of type MyLinkedList

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
 import java.util.EmptyStackException;
 public class MyLinkedListStack<T> {
+    private LinkedList<T> linkStack; //Declaration of an LinkedList to hold the elements of the stack.
 }
