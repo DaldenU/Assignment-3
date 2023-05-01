e3d260ba96d862b1b1b1287808b6de2ac51c5143
DaldenU <daldenuteryag@gmail.com>
2023-05-01 18:23:58 +0600

    testing the methods of MyArrayListStack

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
+        //Creating MyArrayListStack object
+        MyArrayListStack<Integer> arrayStack = new MyArrayListStack<>();
+
+        //adding elements with the "push" method
+        arrayStack.push(5);
+        arrayStack.push(7);
+        arrayStack.push(9);
+        //outputting the size of stack
+        System.out.println(arrayStack.size()); //prints 3
+        //checking whether the stack is empty
+        System.out.println(arrayStack.isEmpty()); //prints false
+        //getting the top element of the stack, while removing it
+        System.out.println(arrayStack.pop()); //prints 9
+        //getting the top element of the stack, without removing it
+        System.out.println(arrayStack.peek()); //prints 7
+
+
     }
 }
