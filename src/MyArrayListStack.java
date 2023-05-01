577de20723914f72517091463b29364818efd912
DaldenU <daldenuteryag@gmail.com>
2023-05-01 18:02:15 +0600

    Creating a constructor and fixing a mistake

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -2,5 +2,10 @@ import java.util.ArrayList;
 import java.util.EmptyStackException;
 
 public class MyArrayListStack<T> {
-    private MyArrayListStack<T> arrStack;
+    private ArrayList<T> arrStack;
+
+    public MyArrayListStack() {
+        arrStack = new ArrayList<>();
+    }
+
 }
