3a32337d7d89f13e4dd66a1f7692440d53452596
DaldenU <daldenuteryag@gmail.com>
2023-05-01 18:31:36 +0600

    push method

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -4,6 +4,10 @@ public class MyLinkedListStack<T> {
     private LinkedList<T> linkStack; //Declaration of an LinkedList to hold the elements of the stack.
 
     public MyLinkedListStack() {
-        linkStack = new LinkedList<>(); ////Initialisation of LinkedList in the constructor
+        linkStack = new LinkedList<>(); //Initialisation of LinkedList in the constructor
+    }
+
+    public void push(T element) {
+        linkStack.addFirst(element); ////adds an element to the top of the stack
     }
 }
