a66ece9367700934ebdd612e021665b922ab5d59
DaldenU <daldenuteryag@gmail.com>
2023-05-01 18:57:18 +0600

    dequeue method

--- src/MyArrayListQueue.java
+++ src/MyArrayListQueue.java
@@ -14,4 +14,13 @@ public class MyArrayListQueue<T> {
     public boolean isEmpty() {
         return queue.isEmpty(); //returns true if the queue is empty, otherwise false
     }
+
+    public T dequeue() {
+        if (isEmpty()) { //checks for whether the queue is empty
+            throw new NoSuchElementException(); //throws an exception if true
+        }
+        T element = queue.get(0); //stores the front element
+        queue.remove(0); //removes the front element
+        return element; //returns the front element
+    }
 }
