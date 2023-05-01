cf7e98b561ab4970df879b80cc6ce744102e4d3d
DaldenU <daldenuteryag@gmail.com>
2023-05-01 18:51:32 +0600

    Declare an instance variable of type MyArrayList

--- src/MyArrayListQueue.java
+++ src/MyArrayListQueue.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
 import java.util.NoSuchElementException;
 public class MyArrayListQueue<T> {
-
+    private ArrayList<T> queue; // Declare an ArrayList to hold the elements of the queue.
 }
