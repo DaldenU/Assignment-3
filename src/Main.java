19076739ab031339b24ec18898d9ade3a467c3ad
DaldenU <daldenuteryag@gmail.com>
2023-05-01 18:43:27 +0600

    testing methods

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         //Creating MyArrayListStack object
         MyArrayListStack<Integer> arrayStack = new MyArrayListStack<>();
 
@@ -16,6 +17,25 @@ public class Main {
         System.out.println(arrayStack.pop()); //prints 9
         //getting the top element of the stack, without removing it
         System.out.println(arrayStack.peek()); //prints 7
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        //Creating MyLinkedListStack object
+        MyLinkedListStack<String> linkedListStack = new MyLinkedListStack<>();
+
+        //adding elements with the "push" method
+        linkedListStack.push("the");
+        linkedListStack.push("you");
+        linkedListStack.push("tree");
+        //outputting the size of stack
+        System.out.println(linkedListStack.size()); //prints 3
+        //checking whether the stack is empty
+        System.out.println(linkedListStack.isEmpty()); //prints false
+        //getting the top element of the stack, while removing it
+        System.out.println(linkedListStack.pop()); //prints "tree"
+        //getting the top element of the stack, without removing it
+        System.out.println(linkedListStack.peek()); //prints "you"
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
     }
